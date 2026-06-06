2328be46eff06830b3b51b269a17157a288ca99b
victoriocarvalho <victorio.carvalho@gmail.com>
2026-06-06 12:16:38 -0300

    Versao 2 - persistencia PostgreSQL com JDBC

--- src/main/java/br/com/cadastroempregados/App.java
+++ src/main/java/br/com/cadastroempregados/App.java
@@ -1,7 +1,8 @@
 package br.com.cadastroempregados;
 
 import br.com.cadastroempregados.aplicacao.EmpregadoService;
-import br.com.cadastroempregados.persistencia.EmpregadoRepositoryMemoria;
+import br.com.cadastroempregados.persistencia.ConexaoFactory;
+import br.com.cadastroempregados.persistencia.EmpregadoRepositoryPostgres;
 import br.com.cadastroempregados.ui.EmpregadoFrame;
 
 import javax.swing.SwingUtilities;
@@ -9,7 +10,7 @@ import javax.swing.SwingUtilities;
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            EmpregadoRepositoryMemoria repository = new EmpregadoRepositoryMemoria();
+            EmpregadoRepositoryPostgres repository = new EmpregadoRepositoryPostgres(new ConexaoFactory());
             EmpregadoService service = new EmpregadoService(repository);
             EmpregadoFrame frame = new EmpregadoFrame(service);
             frame.setVisible(true);
